25f8a01217fa3f2f278b2b30fee8e9559e47e8c7
pb.zhao <pb.zhao@startiasoft.com>
2024-11-06 08:50:36 +0800

    添加aes 加密

--- src-tauri/src/tools/aes.rs
+++ src-tauri/src/tools/aes.rs
@@ -1,6 +1,5 @@
 use aes::cipher::{KeyIvInit, block_padding::Pkcs7, BlockEncryptMut};
 use base64::{Engine as _, engine::general_purpose};
-use bytebuffer::ByteBuffer;
 use rand::distributions::{Alphanumeric, DistString};
 
 type Aes256CbcEnc = cbc::Encryptor<aes::Aes256>;
@@ -11,12 +10,10 @@ pub fn encrypt(key: &str, data: &str) -> (String, String) {
     let iv_str = Alphanumeric.sample_string(&mut rng, 16);
     let iv = iv_str.as_bytes();
     
+    // 加密数据但不拼接 IV 和密文
     let cipher = Aes256CbcEnc::new(key.as_bytes().into(), iv.into())
         .encrypt_padded_vec_mut::<Pkcs7>(data.as_bytes());
-    
-    let mut buffer = ByteBuffer::from_bytes(iv);
-    buffer.write_bytes(&cipher);
-    
-    // 返回 IV 和加密后的数据
-    (iv_str, general_purpose::STANDARD.encode(buffer.as_bytes()))
+
+    // 返回 IV 和 Base64 编码的密文
+    (iv_str, general_purpose::STANDARD.encode(&cipher))
 }
